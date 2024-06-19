ce20f8d020b0ad8e896a99826cd58bccf3c4a2cf
P@ul <MyEmail@Zoho.com>
2024-06-20 00:38:32 +0530

    feat - Navbar items alignment fixed

--- nerdlets/home/Components/Navbar.js
+++ nerdlets/home/Components/Navbar.js
@@ -5,7 +5,7 @@ import Modalbox from './Modal/Modalbox';
 function TopBar() {
     return (
         <Navbar bg="light" data-bs-theme="light" sticky='top'>
-            <Container >
+            <Container fluid className='margin-left-right'>
                 <Navbar.Brand style={{ fontWeight: "800", lineHeight: "3rem" }}>GCC Monitor 3 - User Journeys and Tier 1 App</Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text >
