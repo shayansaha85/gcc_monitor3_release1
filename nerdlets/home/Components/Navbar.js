23b6c9a5b35b3cd7235820e5d162943bf1d63652
shayansaha85 <shayan851997@gmail.com>
2024-06-20 01:04:01 +0530

    edited name and updated icon for the R2

--- nerdlets/home/Components/Navbar.js
+++ nerdlets/home/Components/Navbar.js
@@ -6,7 +6,7 @@ function TopBar() {
     return (
         <Navbar bg="light" data-bs-theme="light" sticky='top'>
             <Container fluid className='margin-left-right'>
-                <Navbar.Brand style={{ fontWeight: "800", lineHeight: "3rem" }}>GCC Monitor 3 - User Journeys and Tier 1 App</Navbar.Brand>
+                <Navbar.Brand style={{ fontWeight: "800", lineHeight: "3rem" }}>GCC Monitor 3 - Tier 1 Apps & User Journey</Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text >
                         <Modalbox />
