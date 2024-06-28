8ff8f713f924626e19eca85085ff39c69374dcd1
P@ul <MyEmail@Zoho.com>
2024-06-28 15:59:44 +0530

    fix - modal box height fixed

--- nerdlets/home/Components/Modal/Modalbox.js
+++ nerdlets/home/Components/Modal/Modalbox.js
@@ -19,6 +19,8 @@ function Modalbox() {
                     <Modal.Title>About Attributes</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <h5 style={{ fontWeight: 'bold' }}>About Attributes</h5>
+                    <br></br>
                     <p>
                         <strong>  Metric #1 Infrastructure Health:</strong> Latest status value of the workload where all infrastructure hosts are added for each application/user journey.
                     </p>
@@ -42,7 +44,16 @@ function Modalbox() {
                     </p>
                     <p>
                         <strong>  Alerts:</strong> Total number of open alerts for the application/user journey.
-                    </p>
+                    </p><br></br>
+                    <h5 style={{ fontWeight: 'bold' }} >Other Information</h5>
+                    <p><strong>About Status:</strong> The color of the widget title bar is determined by the most recent status of the master workload associated with the specific application or user journey.</p>
+
+                    <p><strong>Host Status for 3h:</strong> This status remains static and is calculated from the present time, representing the status of the infrastructure workload for the specified application or user journey over the past three hours.</p>
+
+                    <p><strong>Arrow Direction and Arrow Color: </strong>
+                        The <strong>Arrow Direction</strong> is based on a comparison of the attribute values within the selected time range, as chosen by the user through the embedded timepicker, and the <strong>Arrow Color</strong> is determined by whether the attribute values exceed the predefined critical or warning thresholds. These thresholds can be viewed by hovering the mouse over the values in each widget, where applicable.</p>
+
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
